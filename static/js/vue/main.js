4bc80b9f2a699d1bc56faf3ada8e6ce0cc626692
aaaasmile <testhaus@gmx.at>
2020-03-30 01:08:52 +0200

    update template file

--- static/js/vue/main.js
+++ static/js/vue/main.js
@@ -9,21 +9,22 @@ export const app = new Vue({
         this.Buildnr = window.buildnr
     },
     template: `
-    <v-app><v-content>
-        <div class="home">
-            <h2>Testing Vuetify</h2>
-            <v-btn>Click Me</v-btn>
-            <v-btn color="pink">Rosa</v-btn>
-            <v-btn class="pink white--text">
-                <v-icon left small>email</v-icon>
-            </v-btn>
-            <v-btn fab dark small depressed color="purple">
-                <v-icon dark>favorite</v-icon>
-            </v-btn>
-        </div>
-        <p>Buildnr: {{Buildnr}}</p>
-    </v-content></v-app>
-    `
+  <v-app>
+   <v-content>
+      <div class="home">
+        <h2>Testing</h2>
+        <v-btn color="pink">Rosa o bella</v-btn>
+        <v-btn>Cliccami</v-btn>
+        <v-btn class="pink white--text">
+          <v-icon left small>email</v-icon>
+        </v-btn>
+        <v-btn fab dark small depressed color="purple">
+          <v-icon dark>favorite</v-icon>
+        </v-btn>
+      </div>
+      <p>Buildnr: {{Buildnr}}</p>
+    </v-content> 
+  </v-app>`
 })
 
 console.log('Main is here!', window.buildnr)
---
+++ static/js/vue/main.vue
@@ -0,0 +1,19 @@
+<!-- my-component.vue -->
+<template>
+  <v-app>
+   <v-content>
+      <div class="home">
+        <h2>Testing</h2>
+        <v-btn color="pink">Rosa o bella</v-btn>
+        <v-btn>Cliccami</v-btn>
+        <v-btn class="pink white--text">
+          <v-icon left small>email</v-icon>
+        </v-btn>
+        <v-btn fab dark small depressed color="purple">
+          <v-icon dark>favorite</v-icon>
+        </v-btn>
+      </div>
+      <p>Buildnr: {{Buildnr}}</p>
+    </v-content> 
+  </v-app>
+</template>
