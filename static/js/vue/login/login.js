e89c7a5674b46a593b6db24fa9d618394e8a5aaa
aaaasmile <testhaus@gmx.at>
2023-03-25 00:18:53 +0100

    icons

--- static/js/vue/login/login.js
+++ static/js/vue/login/login.js
@@ -8,6 +8,9 @@ export default {
     console.log("Login component mounted.")
   },
   template: `
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
-`
+  <v-col>
+    <button v-on:click="count++">You clicked me {{ count }} times.</button>
+    <v-icon icon="mdi:mdi-plus" />
+    <v-icon icon="mdi:mdi-minus" />
+  </v-col>`
 }
--- static/js/vue/login/login.vue
+++ static/js/vue/login/login.vue
@@ -1,3 +1,7 @@
 <template>
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
+  <v-col>
+    <button v-on:click="count++">You clicked me {{ count }} times.</button>
+    <v-icon icon="mdi:mdi-plus" />
+    <v-icon icon="mdi:mdi-minus" />
+  </v-col>
 </template>
